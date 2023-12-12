9a36c5f58b2710b34f0e00dbe6ca998dcb7f976f
Lambrosgesu <lambros.gesu@gmail.com>
2023-12-12 17:27:49 +0200

    Microservices Communication using WebClient method

--- employee-service/src/main/java/net/guides/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/guides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,15 @@
 package net.guides.employeeservice.service.impl;
 
+import lombok.AllArgsConstructor;
 import net.guides.employeeservice.dto.APIResponseDto;
 import net.guides.employeeservice.dto.DepartmentDto;
 import net.guides.employeeservice.dto.EmployeeDto;
-import lombok.AllArgsConstructor;
 import net.guides.employeeservice.entity.Employee;
 import net.guides.employeeservice.mapper.EmployeeMapper;
 import net.guides.employeeservice.repository.EmployeeRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import net.guides.employeeservice.service.EmployeeService;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -18,7 +17,7 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
-    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -34,11 +33,15 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         Employee employee = employeeRepository.findById(employeeId).get();
 
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
         APIResponseDto apiResponseDto = new APIResponseDto();
         apiResponseDto.setEmployee(EmployeeMapper.mapToEmployeeDto(employee));
-        apiResponseDto.setDepartment(responseEntity.getBody());
+        apiResponseDto.setDepartment(departmentDto);
         return apiResponseDto;
     }
 }
