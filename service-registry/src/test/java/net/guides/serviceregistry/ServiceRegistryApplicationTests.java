6a90d3c40620368d89510a017a38a854bdfbc02b
Lambrosgesu <lambros.gesu@gmail.com>
2023-12-13 14:34:44 +0200

    Service Registry and Discovery using Spring Cloud Netflix Eureka

---
+++ service-registry/src/test/java/net/guides/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package net.guides.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
